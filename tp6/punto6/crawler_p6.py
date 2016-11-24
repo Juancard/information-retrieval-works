793e365d153027307b9d7062203488a76bea1a6e
juan <juancruz.23@hotmail.com>
2016-11-24 14:27:59 -0300

    crawler p6 actualizado usando funciones de crawler del punto 7

--- tp6/punto6/crawler_p6.py
+++ tp6/punto6/crawler_p6.py
@@ -3,7 +3,8 @@ import sys
 import os
 import re
 import urllib2
-from BeautifulSoup import BeautifulSoup
+import BeautifulSoup as bs
+import urlparse
 
 def getUrl():
 	try:
@@ -19,12 +20,42 @@ def getUrl():
 def main():
 	urlGiven = getUrl()
 	print "Url dada: ", urlGiven
+	print "Links hallados: \n","\n".join(getLinks(urlGiven))
 
-	print "Links hallados: "
-	htmlPage = urllib2.urlopen(urlGiven)
-	soup = BeautifulSoup(htmlPage)
-	for link in soup.findAll('a'):
-	    print link.get('href')
+def getLinks(url, url_instance = None, n = None):
+	extractedLinks = []
+
+	if url_instance is not None:
+		urlContent = url_instance
+	else:
+		try:
+			urlContent = urllib2.urlopen(url)
+		except (ValueError, urllib2.HTTPError, urllib2.URLError) as e:
+			return []
+
+	# Si no es html, no tiene sentido buscar tags <a>, nunca los va a haber
+	if not 'text/html' in urlContent.info().getheader('Content-Type'):
+		return []
+
+	soup = bs.BeautifulSoup(urlContent)
+	for linkTag in soup.findAll('a'):
+		actualLink = linkTag.get('href')
+		# Ignoro links sin href
+		if actualLink is not None:
+			# Ignoro links que comienzan con simbolo '#' (secciones)
+			if not actualLink.startswith("#"):
+
+				# Si es link relativo, se arma el link absoluto con url padre
+				if isRelativeUrl(actualLink):
+					actualLink = urlparse.urljoin(url, actualLink)
+
+				# Agrego links a lista de links hijos
+				extractedLinks.append(actualLink)
+
+	return extractedLinks[:n]
+
+def isRelativeUrl(url):
+	return not bool(urlparse.urlparse(url).netloc)
 
 if __name__ == "__main__":
 	main()
