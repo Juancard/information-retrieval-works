6b31a23766d55ca9e5e96f290d4c2f40fec4c763
juan <juancruz.23@hotmail.com>
2016-11-26 17:43:01 -0300

    mismo fix para script python

--- tp3/punto5/coeficiente_spearman.py
+++ tp3/punto5/coeficiente_spearman.py
@@ -1,11 +1,12 @@
 
 # coding: utf-8
 
+# In[1]:
+
 import csv
 import pandas as pd
 import numpy as np
 import os
-import sys
 import re
 
 def getListas():
@@ -23,68 +24,63 @@ def getListas():
             listas.append(got)
     return listas
 
-def coeficienteSpearman(df):
-    n = len(df)
-    df['d'] = df[df.columns[0]] - df[df.columns[1]]
-    df['d_cuadrado'] = np.power(df['d'],2)
-    sum_d_cuadrado = sum(df['d_cuadrado']) 
-    resultado = 1 - ( (6.0 * sum_d_cuadrado) / (n * ( np.power(n,2.0) - 1.0 ) ) )
-    # WARNING: NO ANDA BIEN
-    #if n > 20:
-    #    resultado = resultado / np.sqrt( (1.0 - np.power( resultado,2.0 )) / (n - 2.0) )
-    return resultado 
+def sp(a):
+    CONSTANTE = 6.0 #  valor que se multiplica por sumatoria del cuadrado de las diferencias
+    N = len(a) - 1.0 # posicion maxima del ranking
+    
+    diferencia = a[a.columns[2]] - a[a.columns[3]]
+    diferenciaCuadrado = np.power(diferencia, 2.0)
+    sumDifCuad = sum(diferenciaCuadrado)
+    dividendo = sumDifCuad * CONSTANTE
+    divisor = N * (np.power(N, 2.0) - 1.0)
+
+    if divisor == 0 or dividendo == 0:
+        cs = 1
+    else:
+        cs = 1 - (dividendo / divisor) 
+    
+    return cs
+
+def getTopN(df, n):
+    return df.sort_values(["Id query",df.columns[0]]).groupby("Id query").head(n)
 
 def main():
-    listas = getListas()
-    if not listas:
+
+    listasPath = getListas()
+    if not listasPath:
         sys.exit()
 
-    data = []
+    lista = []
     for i in range(2):
-        data.append(pd.read_csv(listas[i], sep=" ", header = None, names=["Id query", "Q", "Id doc", "Ranking", "Similtud", "Modelo"], index_col=["Id query","Id doc"]))
-        modelo = data[i]['Modelo'].iloc[0]
-        data[i].drop(['Q','Modelo'], axis=1, inplace=True)
-        data[i].columns = ["Ranking - "+modelo, "Similtud - "+modelo]
+        lista.append(pd.read_csv(listasPath[i], sep=" ", header = None, names=["Id query", "Q", "Id doc", "Ranking", "Similtud", "Modelo"], index_col=["Id query","Id doc"]))
+        modelo = lista[i]['Modelo'].iloc[0]
+        lista[i].drop(['Q','Modelo', "Similtud"], axis=1, inplace=True)
+        lista[i].columns = ["Ranking - "+modelo]
+    lista[1].tail()
 
-    df = pd.concat([data[0], data[1]],axis=1,join="outer")
-
-    # Ordeno por iDQUERY y luego por ranking del primero de los dos modelos
-    df = df.reset_index()
-    df = df.sort_values(["Id query",df.columns[2]])
-    df = df.set_index(["Id query", "Id doc"])
-
-    df.to_csv("resultados_queries.csv")
-
-    # Elimino columnas que contienen valor de similtud (no me interesa para clcular spearman)
-    spearman = df.drop(df.columns[[1,3]], axis=1)
-
-    # Remuevo valores NaN (cuando un modelo devuelve un documento pero el otro no)
-    # NaN los relleno con el maximo rank + 1 de cada modelo en cada query
-    fillValues = spearman.max(level="Id query")+1
-    spearman = spearman.fillna(value=fillValues)
-
-    # Creo el data frame resultado en donde muestro el coeficiente obtenido para cada query
-    spearman = spearman.reset_index()
-    df = pd.DataFrame(index=np.arange(min(spearman['Id query']), max(spearman['Id query'])+1), columns=['Coeficiente (n=10)', 'Coeficiente (n=25)', 'Coeficiente (n=50)'], dtype = 'float64')
-    df.index.name = "Id query"
-
-    # A cada index le doy su coeficiente para los primeros 'n' resultados
-    for i in df.index:
+    tops = {
+        10: [],
+        25: [],
+        50: []
+    }
+    for k in tops:
+        # Obtengo top k para ambas listas
+        ranks = [getTopN(df.reset_index(), k).set_index(["Id query", "Id doc"]) for df in lista]
         
-        # creo dataframe 'query' con los datos del query con id 'i'
-        query = spearman[spearman['Id query'] == i]
-        query = query.drop("Id query", axis=1)
-        query = query.set_index(["Id doc"])
-        # Calculo coeficiente
-        df.loc[i,"Coeficiente (n=10)"] = coeficienteSpearman(query.head(n=10)) 
-        df.loc[i,"Coeficiente (n=25)"] = coeficienteSpearman(query.head(n=25)) 
-        df.loc[i,"Coeficiente (n=50)"] = coeficienteSpearman(query.head(n=50)) 
-        
-    # Restauro index 
-    spearman = spearman.set_index(["Id query", "Id doc"])
+        # Concateno listas
+        ranksConcat = pd.concat(ranks, axis=1, join="outer")
 
-    df.to_csv("coeficiente_spearman.csv")
+        # Docs id que se encuentran en una lista y no en otra se rellenan con el rank maximo + 1
+        fillValues = ranksConcat.max(level="Id query") + 1
+        ranksConcat = ranksConcat.fillna(value=fillValues)
+
+        # Calculo coeficiente de spearman
+        tops[k] = ranksConcat.reset_index().groupby(["Id query"]).apply(sp)
+
+    cs = pd.concat([tops[k] for k in sorted(tops)], axis=1,join="outer")
+    cs.columns = ["top%d" % k for k in sorted(tops)]
+
+    cs.to_csv("coeficiente_spearman.csv")
 
 if __name__ == "__main__":
     main()
-
